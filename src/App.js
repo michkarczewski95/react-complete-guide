19d2ae28c6d36d23eb5c20e6ae441255aeafc8e3
Michal Karczewski <mich.karczewski95@gmail.com>
2021-03-02 19:09:14 +0100

    useState()

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
- state = {
+const app = props => {
+const [ personsState, setPersonsState ] = useState({
   persons: [
 { name: 'Michal', age: 25 },
 { name: 'Luiza', age: 24 },
-{name: 'Odin', age: 1 }
+{ name: 'Odin', age: 1 }
   ], 
   otherState:'some othe value'
- }
-switchNameHandler= () => {
-//console.log('Was clicked!');
-// DON'T DO THIS:this.state.persons[0].name='Michael';
-this.setState({
-  persons: [
-  { name: 'Michael', age: 25 },
-  { name: 'Luiza', age: 24 },
-  { name: 'Odin', age: 30 }
-]
- })
-}
+ });
+ const switchNameHandler = () => {
+   //console.los('Was clicked!');
+   //DON'T DO THIS: this.state.persons[0].name = "Michael";
+   setPersonsState({
+     persons : [
+       {name:'Michael', age: 26},
+       {name:'Luiza', age: 24},
+       {name:'Odin', age: 1}
+     ]
+   });
+ };
 
-
-
-  render() {
     return (
       <div className="App">
    <h1>Hi, I'm a React App</h1>
    <p>This is really working!</p>
-   <button onClick={this.switchNameHandler}>Switch Name</button>
-   <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-   <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-   <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+   <button onClick={switchNameHandler}>Switch Name</button>
+   <Person
+    name={personsState.persons[0].name} 
+    age={personsState.persons[0].age} />
+   <Person
+    name={personsState.persons[1].name} 
+    age={personsState.persons[1].age} />
+    My Hobbies: Racing
+   <Person
+    name={personsState.persons[2].name} 
+    age={personsState.persons[2].age} />
       </div>
     );
     //return React.createElement('div',{className:'App'}, React.createElement('h1',null, 'Does this work now?'))
-  }
+  
 }
 
-export default App;
+export default app;
