3ba5149ae91e99c0f9a0c35ed3f2ef5bef4a7458
Michal Karczewski <mich.karczewski95@gmail.com>
2021-03-07 16:26:32 +0100

    Working with Inline Styles

--- src/App.js
+++ src/App.js
@@ -34,11 +34,21 @@ state= {
   }
 
 render () {
+const style= {
+backgroundColor:'white',
+font: 'inherit',
+border:'1px solid blue' ,
+padding:'8px',
+cursor:'pointer'
+};
+  
     return (
       <div className="App">
    <h1>Hi, I'm a React App</h1>
    <p>This is really working!</p>
-   <button onClick={() => this.switchNameHandler('Michal!!')}>Switch Name</button>
+   <button 
+   style={style}
+   onClick={() => this.switchNameHandler('Michal!!')}>Switch Name</button>
    <Person
       name={this.state.persons[0].name} 
       age={this.state.persons[0].age} />
