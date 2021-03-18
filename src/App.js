12c455b21dc8611137d1d15fe3a1d61cc8ae1fe1
Michal Karczewski <mich.karczewski95@gmail.com>
2021-03-18 12:51:48 +0100

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -51,18 +51,12 @@ let persons= null;
 if(this.state.showPersons){
  persons =(
   <div>
-  <Person
-     name={this.state.persons[0].name} 
-     age={this.state.persons[0].age} />
-  <Person
-     name={this.state.persons[1].name} 
-     age={this.state.persons[1].age} 
-   click={this.switchNameHandler.bind(this, 'Michaelo')} 
-     changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
-  <Person
-     name={this.state.persons[2].name} 
-     age={this.state.persons[2].age} />
-     </div>
+    {this.state.persons.map(person=> {
+      return<Person
+       name={person.name}
+        age={person.age}/>
+    })}
+</div>
 
  ) ;
 }
@@ -85,3 +79,4 @@ if(this.state.showPersons){
 }}
 
 export default App;
+
