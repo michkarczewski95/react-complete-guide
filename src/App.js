17f6dc1ab3178e8d7cb2099b14ac518e9baee2b1
michkarczewski95 <78048731+michkarczewski95@users.noreply.github.com>
2021-08-25 23:53:56 +0200

    Child-to-Parent Component Communication

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   //return React.createElement(
   //  'div',
   //  {},
@@ -33,7 +39,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
