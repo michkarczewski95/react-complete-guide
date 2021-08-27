e9284d4948fe80542cd756ce0517f58ab0e6ce2a
michkarczewski95 <78048731+michkarczewski95@users.noreply.github.com>
2021-08-27 23:14:53 +0200

    Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ title: propsTitle, date, amount }) => {
-    //function clickHandler() {}
-    const [title, setTitle] = useState(propsTitle);
-    console.log('ExpenseItem evaluated by React');
-
-    const clickHandler = () => {
-        setTitle('Updated');
-        console.log(title);
-    };
+const ExpenseItem = (props) => {
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={date} />
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${amount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler} >Change Title</button>
+
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
+
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
+
     const filterChangeHandler = selectedYear => {
-        setFilteredYear(setFilteredYear);
+        setFilteredYear(selectedYear);
     };
     return (
         <div>
